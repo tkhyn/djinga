13430584476fdd401d98c597df73839c0c0571b2
thomas <thomas@ksytek.com>
2014-03-24 15:15:21 +1300

    Added 'simple' url tag

---
+++ djinga/ext/urls.py
@@ -0,0 +1,14 @@
+"""
+Extensions for URL reversing
+"""
+
+from _base import SimpleTag
+
+from django.core.urlresolvers import reverse
+
+
+class URLExtension(SimpleTag):
+    tags = set(['url'])
+
+    def tag_func(self, name, *args, **kwargs):
+        return reverse(name, args=args, kwargs=kwargs)
