aca387289e728c5bab2c4d012ec64be0b2176cfa
Thomas Khyn <thomas@ksytek.com>
2014-05-01 10:43:03 +1200

    Added csrf_token extension from coffin

--- djinga/ext/djangotags.py
+++ djinga/ext/djangotags.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 from jinja2 import nodes, contextfunction
 from jinja2.ext import Extension
+from jinja2 import Markup
 
 
 class DjangoTag(Extension):
@@ -41,3 +42,29 @@ class DjangoTag(Extension):
                 'templates.')
         context = dj_template.RequestContext(request, context)
         return dj_template.Template(html).render(context)
+
+
+class CsrfToken(Extension):
+    """
+    Jinja2-version of the ``csrf_token`` tag.
+
+    Adapted from a snippet by Jason Green:
+    http://www.djangosnippets.org/snippets/1847/
+
+    This tag is a bit stricter than the Django tag in that it doesn't
+    simply ignore any invalid arguments passed in.
+
+    [copied from coffin]
+    """
+
+    tags = set(['csrf_token'])
+
+    def parse(self, parser):
+        lineno = parser.stream.next().lineno
+        return nodes.Output([
+            self.call_method('_render', [nodes.Name('csrf_token', 'load')]),
+        ]).set_lineno(lineno)
+
+    def _render(self, csrf_token):
+        from django.template.defaulttags import CsrfTokenNode
+        return Markup(CsrfTokenNode().render({'csrf_token': csrf_token}))
