012d1857cab080810fd70f9eaeb1da229bdce99f
Thomas Khyn <thomas@ksytek.com>
2015-05-10 01:35:43 +1200

    Adds failing context processor test

---
+++ tests/context_processors.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+from .base import TestCase
+
+
+class ContextProcessorsTests(TestCase):
+
+    options = {'context_processors': [
+        'django.template.context_processors.static',
+    ]}
+    template = '{{ STATIC_URL }}'
+
+    def test_static(self):
+        self.assertRender(settings.STATIC_URL)
