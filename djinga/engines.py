b9cd79bd18ce7db048b79238a8348ecd9cdc2d93
Thomas Khyn <thomas@ksytek.com>
2015-05-09 22:39:08 +1200

    Makes loaders work on Django 1.8+ if necessary

--- djinga/engines.py
+++ djinga/engines.py
@@ -1,42 +1,52 @@
+"""
+Defines _env and _init_env when using the jinja2 template loaders either
+from Django < 1.8 or from the old-style template settings
+
+Import templates.engines for Django 1.8+ or create a dummy engines dictionary
+"""
+
+
+import jinja2
+
+from django.template import loader
+from django.conf import settings
+
+
+_env = None
+
+
+def _init_env(**options):
+    """
+    Initialise the environment that is going to be used in the loaders
+    """
+    global _env
+
+    if _env:
+        return
+
+    from .environment import Environment
+
+    template_dirs = [x for ldr_str in settings.TEMPLATE_LOADERS \
+        for x in loader.find_template_loader(ldr_str) \
+                       .get_template_sources('')]
+
+    settings_options = dict(
+        loader=jinja2.FileSystemLoader(template_dirs),
+        extensions=getattr(settings, 'JINJA2_EXTENSIONS', {}),
+        load_from=getattr(settings, 'JINJA2_LOAD_FROM', ()),
+        filters=getattr(settings, 'JINJA2_FILTERS', {}),
+        globals=getattr(settings, 'JINJA2_GLOBALS', {}),
+        **getattr(settings, 'JINJA2_ENV_ARGS', {})
+    )
+    settings_options.update(**options)
+
+    _env = Environment(**settings_options)
+
+
 try:
     from django.template import engines
 except ImportError:
     # hack for django < 1.8
-
-    import jinja2
-
-    from django.template import loader
-    from django.conf import settings
-
-    _env = None
-
-    def _init_env(**options):
-        """
-        Initialise the environment that is going to be used in the loaders
-        """
-        global _env
-
-        if _env:
-            return
-
-        from .environment import Environment
-
-        template_dirs = [x for ldr_str in settings.TEMPLATE_LOADERS \
-            for x in loader.find_template_loader(ldr_str) \
-                           .get_template_sources('')]
-
-        settings_options = dict(
-            loader=jinja2.FileSystemLoader(template_dirs),
-            extensions=getattr(settings, 'JINJA2_EXTENSIONS', {}),
-            load_from=getattr(settings, 'JINJA2_LOAD_FROM', ()),
-            filters=getattr(settings, 'JINJA2_FILTERS', {}),
-            globals=getattr(settings, 'JINJA2_GLOBALS', {}),
-            **getattr(settings, 'JINJA2_ENV_ARGS', {})
-        )
-        settings_options.update(**options)
-
-        _env = Environment(**settings_options)
-
 
     class DummyEngine(object):
         def __init__(self):
