48819eeec25af6d6b18110880ae89354235839ef
Thomas Khyn <thomas@ksytek.com>
2020-03-01 14:54:51 +1300

    chore(version): use git instead of hg in alpha version parser

--- djinga/version.py
+++ djinga/version.py
@@ -4,7 +4,7 @@ Project versionning info
 
 import subprocess
 
-__version_info__ = (2, 1, 2, 'final', 0)
+__version_info__ = (2, 1, 3, 'alpha', 0)
 
 
 def get_version(version=__version_info__):
@@ -21,14 +21,15 @@ def get_version(version=__version_info__):
         return version_str
 
     if version[3:] == ('alpha', 0):
-        return '%s.dev%s' % (version_str, get_hg_chgset())
+        return '%s.dev0+%s' % (version_str, get_git_chgset())
     else:
         return ''.join((version_str, dev_st[version[3]], str(version[4])))
 
 
-def get_hg_chgset():
+def get_git_chgset():
     try:
-        return subprocess.check_output(['hg', 'id', '-i']).strip()
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'],
+                                       universal_newlines=True).strip()[:-1]
     except:
         return '?'
 
